5fd492bc2ee849fc777a93f0f1eaf500d7ef5e73
Thanh Lan <lanvanthanh54@gmail.com>
2025-11-23 20:41:15 +0700

    Setup Environment

---
+++ ECommerceMVC/Program.cs
@@ -0,0 +1,43 @@
+using ECommerceMVC.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceMVC
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<EcomerceContext>(option =>
+            {
+                option.UseSqlServer(builder.Configuration.GetConnectionString("Ecomerce"));
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
